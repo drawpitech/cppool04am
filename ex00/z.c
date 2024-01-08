22d8cd0aa2e2ea9a3a59eea347e586417d1200fd
Clément <clement2104.boillot@gmail.com>
2024-01-08 09:24:59 +0100

    Add 0th task

---
+++ ex00/z.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** ppool04am
+** File description:
+** z
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("z\n");
+    return EXIT_SUCCESS;
+}
