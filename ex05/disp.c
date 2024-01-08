b699f92409ba8d4a0c3f49792e678a6a325e7ce6
Clément <clement2104.boillot@gmail.com>
2024-01-08 14:25:53 +0100

    Add end of 5th task

--- ex05/disp.c
+++ ex05/disp.c
@@ -5,10 +5,21 @@
 ** disp
 */
 
+#include <stdint.h>
 #include <stdio.h>
 
 void disp_int_array(const int *array, size_t nmemb)
 {
     for (size_t i = 0; i < nmemb; i++)
         printf("%d\n", array[i]);
+}
+
+void disp_array(
+    const void *array, size_t nmemb, size_t size,
+    void (*print)(const void *))
+{
+    const uint8_t *ptr = array;
+
+    for (size_t i = 0; i < nmemb; i++)
+        print(ptr + size * i);
 }
--- ex05/uniq.c
+++ ex05/uniq.c
@@ -6,30 +6,50 @@
 */
 
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 static
-bool is_in_arr(const int *array, size_t nmemb, int value)
+bool is_in_arr(
+    const void *array, size_t nmemb, size_t size,
+    int (*compar)(const void *, const void *))
 {
+    const uint8_t *ptr = array;
+
     for (size_t i = 0; i < nmemb; i++)
-        if (array[i] == value)
+        if (compar(ptr + i * size, ptr + nmemb * size) == 0)
             return true;
     return false;
 }
 
-size_t uniq_int_array(int *array, size_t nmemb)
+static
+int compare_int(const void *a, const void *b)
 {
-    int *dup = malloc(sizeof(int) * nmemb);
+    return (*(const int *)a - *(const int *)b);
+}
+
+size_t uniq_array(
+    void *array, size_t nmemb, size_t size,
+    int (*compar)(const void *, const void *))
+{
+    uint8_t *ptr = array;
+    uint8_t *dup = malloc(size * nmemb);
     size_t i = 0;
 
-    memcpy(dup, array, sizeof(int) * nmemb);
+    memcpy(dup, array, size * nmemb);
     for (size_t j = 0; j < nmemb; j++) {
-        if (!is_in_arr(dup, j, array[j])) {
-            array[i] = array[j];
-            i += 1;
-        }
+        if (is_in_arr(dup, j, size, compar))
+            continue;
+        for (size_t x = 0; x < size; x++)
+            ptr[i * size + x] = ptr[j * size + x];
+        i += 1;
     }
     return i;
+}
+
+size_t uniq_int_array(int *array, size_t nmemb)
+{
+    return uniq_array(array, nmemb, sizeof(int), &compare_int);
 }
